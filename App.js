6f98e5dad0f1e21cf788a86ac824944cdc7b8ea5
himegitaichi <himetai0625@icloud.com>
2024-09-01 16:46:51 +0900

    ポケモンデータの取得

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { getAllPokemon } from './utils/pokemon';
 
 function App() {
-  const intialURL = "https://pokeapi.co/api/v2/pokemon" //エンドポイントを追加
+  const intialURL = "https://pokeapi.co/api/v2/pokemon"; //エンドポイントを追加
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       //全てのポケモンデータを取得
       let res = await getAllPokemon(intialURL);
-      console.log(res);
+      //各ポケモンの詳細なデータを取得
+      loadPokemon(res.results);
+      console.log(res.results);
+      setLoading(false);
     };
     fetchPokemonData();
   },[]);
 
+  const loadPokemon = () => {};
+
   return (
     <div className="App">
+      {loading ?(
+        <h1>ロード中・・・</h1>
+      ) : (
+      <>
+        <h1>ポケモンデータを取得しました</h1>
+      </>
+      )}
     </div>
   );
 }
